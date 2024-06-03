83c160501194e41783356a70526d88941095e794
hpdzz <tranlehuuphuoc22502@gmail.com>
2024-06-03 22:34:47 +0700

    userSchema, login,signup,middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,18 @@
+const createError = (statusCode, message) => {
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+};
+
+module.exports = { createError, errorHandler };
